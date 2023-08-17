3e8a2820d85e84b1b9b75ae6f7bf67426c274de1
Jim Jin <jim_king_2000@163.com>
2023-08-17 18:02:05 +0800

    add video player

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,28 @@
 'use client';
 
-import { Text, Box, Nav, Menu, Select, Sidebar, Accordion, AccordionPanel, List, CheckBox, Button } from 'grommet';
-import { CloudComputer, User, Add, Logout, SettingsOption, Video, Webcam } from 'grommet-icons';
+import { Text, Box, Nav, Menu, Select, Sidebar, List, CheckBox, Button, Main, Grid, Video, Stack } from 'grommet';
+import { CloudComputer, User, Add, Logout, SettingsOption, Webcam, Expand, Camera, ZoomIn, VolumeMute, Microphone, Close, Shift } from 'grommet-icons';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-
+import { useRef } from 'react';
+import {
+	ScreenCopyControl,
+	ChatControl,
+	MuteControl,
+	RecordControl,
+	PanLayer,
+	ResolutionControl,
+	ZoomControl,
+} from '../components/Components';
 
 const data0 = [
   { label: '客厅' }, 
   { label: '走廊' }, 
   { label: '厨房', disabled: true }];
-const data1 = [
-  { label: '192.168.100.10' }, 
-  { label: '192.168.100.11' }, 
-  { label: '192.168.100.12' }];
-const data2 = [
-  { label: '192.168.200.10' }, 
-  { label: '192.168.200.11' }, 
-  { label: '192.168.200.12' }];
 
-export function CameraCheckBox({ disabled, isNew, checked, label }) {
+export function CameraCheckBox({ disabled,  checked, label }) {
   return (
-    <CheckBox disabled={disabled} checked={checked} label={<Button plain label={label} icon={isNew ? <Webcam /> : <Video />} />} />
+    <CheckBox disabled={disabled} checked={checked} label={<Button plain label={label} icon={<Webcam />} />} />
   );
 }
 
@@ -30,6 +31,39 @@ function CameraList({ isNew, data, onClickCameraSetting }) {
     <List data={data} border={false} itemKey='label' action={(item) => <Button disabled={item.disabled} key={item.label} plain tip='设置' icon={<SettingsOption />} onClick={() => onClickCameraSetting(item.label)} />}>
       {datum => <CameraCheckBox disabled={datum.disabled} isNew={isNew} label={datum.label} key={datum.label} />}
     </List>
+  );
+}
+
+function IconButton({ content, icon, ...prop }) {
+  return <Button plain tip={content} icon={icon} {...prop} />;
+}
+
+const BigViewPort = { gridColumn: 'span 2', gridRow: 'span 2' };
+
+export function VideoPlayer({ ...prop }) {
+  const ref = useRef();
+  return (
+    <Box fill background='background' {...prop} ref={ref}>
+      <Stack fill interactiveChild='last'>
+        <Video controls={false} />
+        <Stack fill>
+          <Box fill justify='between'>
+            <Box pad='small' direction='row' justify='end'>
+              <IconButton key={5} content='关闭' icon={<Close />} />
+            </Box>
+            <Box direction='row' justify='center' gap='small'>
+              <ScreenCopyControl showTitle={false} />
+              <RecordControl showTitle={false} />
+              <ChatControl showTitle={false} />
+              <MuteControl showTitle={false} />
+              <PanLayer target={ref} />
+              <ResolutionControl showTitle={false} />
+              <ZoomControl showTitle={false} />
+            </Box>
+          </Box>
+        </Stack>
+      </Stack>
+    </Box>
   );
 }
 
@@ -43,22 +77,19 @@ export default function Page() {
         </Box>
         <Select options={['简体中文', 'English']} value='简体中文' />
         <Menu label={<User />} items={[
-          {label: '添加设备', icon: <Add />, gap: 'small', onClick: () => setShowQRCode(!showQRCode)},
+          {label: '账号设置', icon: <Add />, gap: 'small', onClick: () => setShowQRCode(!showQRCode)},
           {label: '退出登录', icon: <Logout />, gap: 'small'},
         ]} />
       </Nav>
-      <Box direction='row' flex={{ grow: 1, shrink: 1 }} background='background-front'>
+      <Box direction='row' flex={{ grow: 1, shrink: 1 }}>
         <Sidebar flex={false} width='medium'>
-          <Accordion>
-            <AccordionPanel label='IPC Proxy 1'>
-              <CameraList data={data1} onClickCameraSetting={() => {}} />
-            </AccordionPanel>
-            <AccordionPanel label='IPC Proxy 2'>
-              <CameraList data={data2} onClickCameraSetting={() => {}} />
-            </AccordionPanel>
-            <CameraList isNew data={data0} onClickCameraSetting={() => {}} />
-          </Accordion>
+          <CameraList isNew data={data0} onClickCameraSetting={() => {}} />
         </Sidebar>
+        <Main pad='small' background='background-front'>
+          <Grid fill columns='640px' gap='small' align='center' justify='center' style={{ gridAutoFlow: 'row dense', gridAutoRows: '480px' }}>
+            {Array.from({ length: 20 }, (_, index) => index).map(item => <VideoPlayer key={item} />)}
+          </Grid>
+        </Main>
       </Box>
     </Box>
   );
