1021b17f476dbbc64531874fd21bcc094348761c
Jim Jin <jim_king_2000@163.com>
2023-09-14 14:50:36 +0800

    modify sidebar

---
+++ app/[locale]/layout.jsx
@@ -0,0 +1,26 @@
+import { NextIntlClientProvider } from "next-intl";
+import { notFound } from "next/navigation";
+import GrommetRoot from "@/jujiuuicomponents/layout/grommet";
+
+export function generateStaticParams() {
+  return [{ locale: "en" }, { locale: "cn" }];
+}
+
+export default async function RootLayout({ children, params: { locale } }) {
+  let messages;
+  try {
+    messages = (await import(`@/messages/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
+
+  return (
+    <html>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <GrommetRoot>{children}</GrommetRoot>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,3 @@
-import { Grommet, grommet } from 'grommet';
-import StyledComponentsRegistry from '@/jujiuuicomponents/layout/registry';
-
 export default function RootLayout({ children }) {
-  return (
-    <html>
-      <body>
-        <StyledComponentsRegistry>
-          <Grommet full theme={grommet} themeMode='dark'>
-            {children}
-          </Grommet>
-        </StyledComponentsRegistry>
-      </body>
-    </html>
-  )
+  return children;
 }
