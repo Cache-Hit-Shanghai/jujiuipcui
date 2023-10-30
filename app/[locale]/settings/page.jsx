be2e6cbb5d3f447873d7296fc5e4444ddae7a49e
Jim Jin <jim_king_2000@163.com>
2023-10-30 14:50:54 +0800

    refactory

--- app/[locale]/settings/page.jsx
+++ app/[locale]/settings/page.jsx
@@ -10,13 +10,13 @@ export default function Page() {
 		<Main background='background' pad='medium' flex={{ grow: 1, shrink: 1 }}>
 			<Heading level={2}>{t('系统设置')}</Heading>
 			<Tabs alignControls='start'>
-				<Tab title={t('账号设置')}>
+				<Tab title={t('账号设置')} margin={{ bottom: 'small' }}>
 					<AccountSettings />
 				</Tab>
-				<Tab title={t('通知设置')}>
+				<Tab title={t('通知设置')} margin={{ bottom: 'small' }}>
 					<NotificationSettings />
 				</Tab>
-				<Tab title={t('界面设置')}>
+				<Tab title={t('界面设置')} margin={{ bottom: 'small' }}>
 					<UiSettings />
 				</Tab>
 			</Tabs>
